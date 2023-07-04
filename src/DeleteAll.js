e84e515c0d045e971fee4943881e3767557002f9
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-07-05 00:31:54 +0300

    fix: make deleteAll component shorter

--- src/DeleteAll.js
+++ src/DeleteAll.js
@@ -1,19 +1,12 @@
 import React from "react"
 
 function DeleteAll({ noBtn, yesBtn }) {
-  const handleYes = () => {
-    yesBtn()
-  }
-
-  const handleNo = () => {
-    noBtn()
-  }
   return (
     <div className='deleteAll__container'>
       <h2>Do you really want to delete all tasks</h2>
       <div className='deleteAll__buttons'>
-        <button onClick={() => handleNo()}>No</button>
-        <button onClick={() => handleYes()}>Yes</button>
+        <button onClick={() => noBtn()}>No</button>
+        <button onClick={() => yesBtn()}>Yes</button>
       </div>
     </div>
   )
