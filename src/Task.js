6a5c1755be2c82a1d3713d0796c9b71493fc8a28
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-07-11 02:33:18 +0300

    feat: complete check task funcionality

--- src/Task.js
+++ src/Task.js
@@ -8,17 +8,20 @@ import {
 
 function Task({ name, task_id, deleteSingleTask }) {
   const [showID, setShowID] = useState(false)
+  const [check, setCheck] = useState(false)
 
   const handleDelBtn = (id) => {
     deleteSingleTask(id)
   }
   return (
     <div className='single__task' key={task_id}>
-      <h2>{name}</h2>
+      <h2 className={check ? "overwrite-effect" : "single__task-title"}>
+        {name}
+      </h2>
       <h3 className={showID ? "task-id" : "task-id-disable"}>{task_id}</h3>
       <div className='task__icons'>
-        <button id='btn-check'>
-          <BsSquare />
+        <button id='btn-check' onClick={() => setCheck(!check)}>
+          {check ? <BsCheckSquare /> : <BsSquare />}
         </button>
         <button id='btn-delete' onClick={() => handleDelBtn(task_id)}>
           <BsFillTrashFill />
